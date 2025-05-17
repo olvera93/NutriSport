fce332349fe37fde730a1b780b93c4b7c2368995
olvera93 <olvera9302@outlook.com>
2025-05-17 01:20:46 -0600

    [ADD] Resources, Colors and Fonts

---
+++ shared/src/commonMain/kotlin/com/nutrisport/shared/Resources.kt
@@ -0,0 +1,46 @@
+package com.nutrisport.shared
+
+import nutrisport.shared.generated.resources.Res
+import nutrisport.shared.generated.resources.*
+
+object Resources {
+
+    object Icon {
+        val Plus = Res.drawable.plus
+        val Minus = Res.drawable.minus
+        val SignIn = Res.drawable.log_in
+        val SignOut = Res.drawable.log_out
+        val Unlock = Res.drawable.unlock
+        val Search = Res.drawable.search
+        val Person = Res.drawable.user
+        val Checkmark = Res.drawable.check
+        val Edit = Res.drawable.edit
+        val Menu = Res.drawable.menu
+        val BackArrow = Res.drawable.back_arrow
+        val RightArrow = Res.drawable.right_arrow
+        val Home = Res.drawable.home
+        val ShoppingCart = Res.drawable.shopping_cart
+        val Categories = Res.drawable.grid
+        val Dollar = Res.drawable.dollar
+        val MapPin = Res.drawable.map_pin
+        val Close = Res.drawable.close
+        val Book = Res.drawable.book
+        val VerticalMenu = Res.drawable.vertical_menu
+        val Delete = Res.drawable.delete
+        //val Warning = Res.drawable.warning
+        val Weight = Res.drawable.weight
+    }
+    object Image {
+        val ShoppingCart = Res.drawable.shopping_cart_image
+        val Checkmark = Res.drawable.checkmark_image
+        val Cat = Res.drawable.cat
+        val GoogleLogo = Res.drawable.google_logo
+        val PaypalLogo = Res.drawable.paypal_logo
+    }
+    object Flag {
+        //val India = Res.drawable.india
+        //val Usa = Res.drawable.usa
+        //val Serbia = Res.drawable.serbia
+    }
+
+}
